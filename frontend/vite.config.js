93e192fd787bc098dc628a20d3ed2b0f20c4c661
sharonba99 <sharonba99@gmail.com>
2026-01-02 19:43:46 +0200

    Fixed vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
-// frontend/vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Use the default Vite root (the frontend folder)
+  
   root: '.',
   build: {
-    // Output to the 'dist' folder
+    
     outDir: 'dist', 
     emptyOutDir: true,
   },
-  // Ensure base paths are correct for Nginx
-  base: '/', 
+  
+  base: '/',
+  test: {
+    globals: true,
+    environment: 'jsdom', 
+  }, 
 });
